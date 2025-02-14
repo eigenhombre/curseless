6335af49c92daaff3fc742c60fef2e235c39fd85
John Jacobsen <eigenhombre@gmail.com>
2025-02-14 17:13:56 -0600

    Rework; working initial map.

--- src/main.lisp
+++ src/main.lisp
@@ -28,12 +28,20 @@
       ;; Restore original settings
       (sb-posix:tcsetattr fd sb-posix:tcsanow orig-termios))))
 
-(defmacro defterrain (name &body mappings)
+(defun seed-random ()
+  (setf *random-state* (make-random-state t)))
+
+(defmacro defterrain (&body mappings)
   `(progn
      ,@(loop for (symbol char-str) on mappings by #'cddr
-             collect `(setq ,symbol (string ,char-str)))))
+             collect `(progn
+                        (defparameter ,symbol nil)
+                        (setq ,symbol (string ,char-str))))))
 
-(defterrain tmap
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf (get 'defterrain 'common-lisp-indent-function) '(1 &body)))
+
+(defterrain
   WOODS "^"
   PLAYER "@"
   FIELD "."
@@ -46,38 +54,113 @@
 (defun terrain (w h)
   (loop for y to h
         collect
-        (apply #'concatenate 'string
-               (loop for x to w
-                     collect (rand-nth (list WOODS
-                                             FIELD
-                                             PIT
-                                             WATER))))))
+        (loop for x to w
+              collect (if (equal 0 (random 5))
+                          (rand-nth (list WOODS
+                                          FIELD
+                                          PIT
+                                          WATER))
+                          FIELD))))
+
+(defun map-with-player (terr w h player-loc)
+  ;; Superimpose the player on the terrain
+  (let ((new-terr (copy-seq terr)))
+    (setf (elt (elt new-terr (cadr player-loc))
+               (car player-loc))
+          PLAYER)
+    new-terr))
+
+(defun map-as-str (terr)
+  (format nil "~{~{~a~}~^~%~}" terr))
+
+(defun up (n)
+  (loop repeat n do
+    (format t "~c[1A" #\escape)  ; Equivalent to "\033[F"
+    (force-output)))
+
+(defun up-one-line () (up 1))
+
+(defun down (n)
+  (loop repeat n do
+    (format t "~C[B" #\Escape))
+  (force-output))
+
+(defun down-one-line () (down 1))
+
+(defun beginning-of-line ()
+  (format t "~c[1G" #\escape))
+
+(defun clear-line ()
+  "Clears the current line in the terminal."
+  (format t "~c[2K" #\escape)  ; Equivalent to "\033[K"
+  (force-output))
+
+(defun back (n)
+  (loop repeat n do
+    (format t "~C[D" #\Escape))
+  (force-output))
+
+(defun back-one-space ()
+  (back 1))
+
+(defun forward (n)
+  (loop repeat n do
+    (format t "~C[C" #\Escape))
+  (force-output))
+
+(defun forward-one-space ()
+  (forward 1))
+
+(defun clear-map (h)
+  (loop repeat h do
+    (up-one-line)
+    (beginning-of-line)
+    (clear-line)))
+
+(defun print-lines (n)
+  (loop repeat n do (format t "~%")))
+
+(defun player-x (loc)
+  (car loc))
+
+(defun player-y (loc)
+  (cadr loc))
 
 (defun draw-map (terr w h player-x player-y)
-  (loop for x to w do
+  (loop for y to h do
     (progn
-      (loop for y to h do
+      (loop for x to w do
         (let ((outc
                 (if (and (equal x player-x)
                          (equal y player-y))
                     PLAYER
                     (elt (elt terr y) x))))
-          (princ (string outc))))
-      (format t "~%"))))
+          ;; Avoid format/escape issue, use princ:
+          (princ (string outc))
+          (force-output)))
+      (when (< y h) (format t "~%")))))
 
 (defun main ()
   (format t "Welcome to rectumon!~%")
-  (loop do
-    (let* ((w (+ 10 (random 40)))
-           (h (+ 7 (random 15)))
-           (player-loc (list (random w)
-                             (random h)))
-           (terr (terrain w h)))
-      (draw-map terr w h (car player-loc) (cadr player-loc)))
-    (format t "Please type a key...")
-    (force-output)
-    (let ((key (read-single-keystroke)))
-      (format t "You typed: ...'~a'...~%" key)
-      (when (char= key #\q)
-        (format t "Thanks again for using rectumon!~%")
-        (return-from main)))))
+  (seed-random)
+  (let* ((w (+ 30 (random 40)))
+         (h (+ 5 (random 10)))
+         (player-loc (list (random w)
+                           (random h)))
+         (terr (terrain w h))
+         (first-time t))
+    (loop do
+      (when first-time
+        (print-lines h))
+      (clear-map h)
+      (princ (map-as-str (map-with-player terr w h player-loc)))
+      (back (1+ (- w (player-x player-loc))))
+      (up (- h (player-y player-loc)))
+      ;; (fresh-line)
+      (setf first-time nil)
+      (let ((key (read-single-keystroke)))
+        (down (- h (player-y player-loc)))
+        (when (char= key #\q)
+          (format t "~%Bye!~%")
+          (return-from main)))
+      (forward (- h (player-y player-loc))))))
